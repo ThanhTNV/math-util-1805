d7787bff31298075cfe7afaf64b15042280338f7
thanhtnv <thanhtran190304@gmail.com>
2024-06-12 17:07:58 +0700

    modify main code to use recursion and to see green 4ever again

--- src/main/java/com/giaolang/mathutil/core/MathUtility.java
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -20,20 +20,33 @@ public class MathUtility {
     //n: 0...20; do 21! tràn kiểu long, ko có âm giai thừa
     //0! = 1;
     public static long getFactorial(int n) {
-    
-        long product = 1; //biến con heo đất, biến nhân dồn, biến tiếp chiêu
-        
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
     
-        if (n == 0)
-            return 1; 
+        if (n == 0 || n == 1)
+            return 1;
         
-        //code chạy đến đây, n = 1...2...20
-        for (int i = 1; i <= n; i++) {
-            product *= i;  //product = product * i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
+        
+        //n! = n * (n - 1)! - LIFO - Last in First out
+        //USING RECURSION TO DEMONSTRATE REGRESSION TEST
     }
     
 }
+
+//HỌC VỀ REGRESSION TEST - KIỂM THỬ HỒI QUY
+//KIỂM THỬ LẠI NHỮNG THỨ ĐÃ TỪNG KIỂM THỬ, TẠI SAO PHẢI LÀM ĐIỀU NÀY
+//LÝ DO: CODE ĐANG ỔN, MÀ KO ĐỤNG VÀO NÓ, THÌ NÓ VẪN ỔN
+//       NHƯNG NẾU CÓ FIX BUG, CÓ SỬA HÀM, CÓ TỐI ƯU THUẬT TOÁN TRONG HÀM
+//       THÌ HÀM CẦN PHẢI ĐC TEST LẠI - TEST LẠI THỨ ĐÃ TỪNG TEST
+//       TEST LẠI HÀM ĐÃ TỪNG TEST, ĐỂ XÁC NHẬN RẰNG NÓ CÒN NGON - CÒN
+//       XANH HAY KHÔNG SAU KHI CODE ĐÃ ĐƯỢC SỬA
+
+//NẾU CÓ UNIT TEST CODE(JUNIT) VIỆC TEST LẠI CODE CỰC NHANH
+//CHỈ CẦN THẤY MÀU XANH CỦA CÁC JUNIT TEST CASE ĐC RUN LÀ ĐỦ
+//NHANH NỮA: ĐƯA UNIT TEST LÊN GITHUB, CÀI CI SCRIPT (FILE YAML/YML) VÀO
+//MỌI THỨ TỰ ĐỘNG BÁO XANH ĐỎ LUÔN
+
+//KIỂM THỬ LẠI NHỮNG THỨ ĐÃ KIỂM THỬ GỌI LÀ TEST HỒI QUY - REGRESSION TEST
+//NÊN DÙNG TỰ ĐỘNG BẰNG CÁCH XÀI UNIT TEST FRAMEWORK
+//NHÌN = MẮT TỪ HÀM MAIN() CÁCH TRUYỀN THỐNG LÀ KHÔNG NÊN
